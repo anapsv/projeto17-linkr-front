2e0d778557de6f79089a281fe15652cc230f081d
ana <anapsilva793@gmail.com>
2022-08-13 14:27:47 -0300

    like feature

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,117 @@
 import styled from "styled-components";
-import { CgHeart } from "react-icons/cg";
+import axios from "axios";
+import { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { IconContext } from "react-icons";
+import { useUserData } from "../contexts/UserDataContext";
+import ReactTooltip from 'react-tooltip';
+import { useEffect } from "react";
 
-export default function Posts() {
+export default function Posts({ publication }) {
+
+  const [isLiked, setIsLiked] = useState(false);
+  const [likes, setLikes] = useState(0);
+  const [{ token }] = useUserData();
+  const [names, setNames] = useState([]);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  useEffect(() => {
+    const promise = axios.get(`http://localhost:4000/like/${publication.id}`, config);
+    const promise2 = axios.get(`http://localhost:4000/like/count/${publication.id}`);
+    const promise3 = axios.get(`http://localhost:4000/likes/names/${publication.id}`, config);
+    promise.then((response) => {
+      if (response.data) {
+        setIsLiked(true);
+      }
+    });
+    promise2.then((response) => {
+      setLikes(Number(response.data.count));
+    });
+
+    promise3.then((response) => {
+      const nameResponse = response.data;
+      setNames(nameResponse);
+    });
+
+    promise.catch((error) => {
+      console.error("error");
+    });
+
+    promise2.catch((error) => {
+      console.error("error");
+    });
+
+    promise3.catch((error) => {
+      console.error("error");
+    });
+  }, [publication]);
+
+  function like() {
+    const promise = axios.post(
+      `http://localhost:4000/like`,
+      {
+        publicationId: publication.id,
+      },
+      config
+    );
+    promise.then((response) => {
+      if (response.status === 201) {
+        setIsLiked(true);
+        setLikes(likes + 1);
+      }
+    });
+
+    promise.catch((error) => {
+      console.error("error");
+    });
+
+    const promise2 = axios.get(
+      `http://localhost:4000/likes/names/${publication.id}`,
+      config
+    );
+    promise2.then((response) => {
+      const nameResponse = response.data;
+      setNames(nameResponse);
+    });
+    promise2.catch((error) => {
+      console.error("error");
+    });
+  }
+
+  function dislike() {
+    const promise = axios.delete(
+      `http://localhost:4000/likes/${publication.id}`,
+      config
+    );
+    promise.then((response) => {
+      if (response.status === 200) {
+        setIsLiked(false);
+        setLikes(likes - 1);
+        const promise2 = axios.get(
+          `http://localhost:4000/likes/names/${publication.id}`,
+          config
+        );
+        promise2.then((response) => {
+          const nameResponse = response.data;
+
+          setNames(nameResponse);
+        });
+        promise2.catch((error) => {
+          console.error("error");
+        });
+      }
+    });
+
+    promise.catch((error) => {
+      console.error("error");
+    });
+  }
+
   return (
     <Container>
       <LikeSection>
@@ -10,10 +119,28 @@ export default function Posts() {
           src="https://ichef.bbci.co.uk/news/976/cpsprodpb/17638/production/_124800859_gettyimages-817514614.jpg"
           alt="profilePic"
         />
-        <IconContext.Provider value={{ color: "white", size: "2em" }}>
-        <CgHeart/>
-        </IconContext.Provider>
-        <p>13 Likes</p>
+        { isLiked ? <IconContext.Provider value={ { color: "red", size: "1.5em" } }><FaHeart onClick={ dislike } /></IconContext.Provider> :
+          <IconContext.Provider value={ { color: "white", size: "1.5em" } }><FaRegHeart onClick={ like } /></IconContext.Provider> }
+        <a data-tip data-for={ `${publication.id}` }>
+          <span>{ likes } likes</span>
+        </a>
+        { isLiked ? (
+          <ReactTooltip id={ `${publication.id}` } place="bottom" type="light">
+            Você
+            { names.length > 0
+              ? `, ${names[0].name} and others ${likes - 2} people`
+              : ` and others 0 people` }
+          </ReactTooltip>
+        ) : (
+          <ReactTooltip id={ `${publication.id}` } place="bottom" type="light">
+            { names.length > 1
+              ? `${names[0].name}, ${names[1].name} and others ${likes - 2
+              } people`
+              : names.length === 1
+                ? `${names[0].name} and others 0 people`
+                : "0 likes" }
+          </ReactTooltip>
+        ) }
       </LikeSection>
       <div>
         <h1>Juvenal Juvêncio</h1>
